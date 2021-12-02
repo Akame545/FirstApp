dccd9310a4c58947527f6417755914bbade69070
AlejandroMartin <alejandro.mf1002@gmail.com>
2021-12-02 09:04:51 +0100

    Inicio funcionalidades main

--- app/src/main/java/com/alexmartin/firstapp/Main.java
+++ app/src/main/java/com/alexmartin/firstapp/Main.java
@@ -3,23 +3,17 @@ package com.alexmartin.firstapp;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
-import android.widget.AdapterView;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.alespero.expandablecardview.ExpandableCardView;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -31,16 +25,21 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView mycontext = (WebView) findViewById(R.id.vistaweb);
-        registerForContextMenu(mycontext);
+//      Creacion del objeto WebView vinculado con el id
+        WebView main_content = (WebView) findViewById(R.id.vistaweb);
 
-        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe);
+//      Registro del menú contextual.
+        registerForContextMenu(main_content);
+
+
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.refresh_content);
         swipeLayout.setOnRefreshListener(mOnRefreshListener);
 
         miVisorWeb = (WebView) findViewById(R.id.vistaweb);
+        miVisorWeb.setInitialScale(220);
+        miVisorWeb.getSettings().setBuiltInZoomControls(false);
+        miVisorWeb.loadUrl("https://thiscatdoesnotexist.com/");
 
-        miVisorWeb.getSettings().setBuiltInZoomControls(true);
-        miVisorWeb.loadUrl("https://thispersondoesnotexist.com");
     }
     public void showAlertDialogButtonClicked(Main mainActivity) {
 
@@ -48,17 +47,14 @@ public class Main extends AppCompatActivity {
         MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
 
 //        //el dialogo estandar tiene título/icono pero podemos sustituirlo por un XML a medida
-        builder.setTitle("Achtung!");
-        builder.setMessage("Where do you go?");
+        builder.setTitle("Hey Listen!!");
+        builder.setMessage("Where do you go");
         builder.setIcon(R.drawable.ic_person);
         builder.setCancelable(false);
 
 
-//        // un XML a medida para el diálogo
-//        builder.setView(getLayoutInflater().inflate(R.layout.alertdialog_view, null));
-
         // add the buttons
-        builder.setPositiveButton("Signup", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("Positive", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 // do something like...
@@ -69,7 +65,7 @@ public class Main extends AppCompatActivity {
             }
         });
 
-        builder.setNegativeButton("Do nothing", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("Negative", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
@@ -79,7 +75,7 @@ public class Main extends AppCompatActivity {
             }
         });
 
-        builder.setNeutralButton("Other", new DialogInterface.OnClickListener() {
+        builder.setNeutralButton("Neutral", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
@@ -119,19 +115,19 @@ public class Main extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.item1) {
+        if (id == R.id.item10) {
 //            showAlertDialogButtonClicked(Main.this);
 
             Toast toast = Toast.makeText(this, "Infecting", Toast.LENGTH_LONG);
             toast.show();
 
         }
-        if (id == R.id.item2) {
+        if (id == R.id.item20) {
             Toast toast = Toast.makeText(this, "Fixing", Toast.LENGTH_LONG);
             toast.show();
         }
 
-        if (id == R.id.item4) {
+        if (id == R.id.item40) {
             showAlertDialogButtonClicked(Main.this);
         }
 
@@ -146,11 +142,11 @@ public class Main extends AppCompatActivity {
 //                        Toast.LENGTH_LONG);
 //                toast.show();
 
-                final ConstraintLayout mLayout =  findViewById(R.id.MainConstraint);
+                ConstraintLayout mLayout =  findViewById(R.id.MainConstraint);
 
                 Snackbar snackbar = Snackbar
                         .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
-                        .setAction("UNDO", new View.OnClickListener() {
+                        .setAction("Deshacer", new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
                                 Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
